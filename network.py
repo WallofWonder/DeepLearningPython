fb6c6c3f4da7e838e34834611281f39f7a9452ab
ZYF <m13110683916@163.com>
2022-04-11 09:27:29 +0800

    理解手写数字识别反向传播 network.py

--- network.py
+++ network.py
@@ -19,6 +19,7 @@ import random
 # Third-party libraries
 import numpy as np
 
+
 class Network(object):
 
     def __init__(self, sizes):
@@ -41,7 +42,7 @@ class Network(object):
     def feedforward(self, a):
         """Return the output of the network if ``a`` is input."""
         for b, w in zip(self.biases, self.weights):
-            a = sigmoid(np.dot(w, a)+b)
+            a = sigmoid(np.dot(w, a) + b)
         return a
 
     def SGD(self, training_data, epochs, mini_batch_size, eta,
@@ -65,12 +66,12 @@ class Network(object):
         for j in range(epochs):
             random.shuffle(training_data)
             mini_batches = [
-                training_data[k:k+mini_batch_size]
+                training_data[k:k + mini_batch_size]
                 for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
             if test_data:
-                print("Epoch {} : {} / {}".format(j,self.evaluate(test_data),n_test))
+                print("Epoch {} : {} / {}".format(j, self.evaluate(test_data), n_test))
             else:
                 print("Epoch {} complete".format(j))
 
@@ -82,12 +83,13 @@ class Network(object):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         for x, y in mini_batch:
-            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
-            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        self.weights = [w-(eta/len(mini_batch))*nw
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)  # 每个样本关于代价函数的w和b方向的梯度
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]  # 关于代价函数的w方向的梯度求和 (∑δaT)
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]  # 关于代价函数的b方向的梯度求和 (∑z)
+        # 梯度下降 书本43页的第二个框的第三步
+        self.weights = [w - (eta / len(mini_batch)) * nw
                         for w, nw in zip(self.weights, nabla_w)]
-        self.biases = [b-(eta/len(mini_batch))*nb
+        self.biases = [b - (eta / len(mini_batch)) * nb
                        for b, nb in zip(self.biases, nabla_b)]
 
     def backprop(self, x, y):
@@ -99,18 +101,17 @@ class Network(object):
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         # feedforward
         activation = x
-        activations = [x] # list to store all the activations, layer by layer
-        zs = [] # list to store all the z vectors, layer by layer
+        activations = [x]  # list to store all the activations, layer by layer
+        zs = []  # list to store all the z vectors, layer by layer
         for b, w in zip(self.biases, self.weights):
-            z = np.dot(w, activation)+b
+            z = np.dot(w, activation) + b  # z = wa + b
             zs.append(z)
-            activation = sigmoid(z)
+            activation = sigmoid(z)  # a = σ(z)
             activations.append(activation)
         # backward pass
-        delta = self.cost_derivative(activations[-1], y) * \
-            sigmoid_prime(zs[-1])
-        nabla_b[-1] = delta
-        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])  # 输出误差，方程[SP1]
+        nabla_b[-1] = delta  # 最后一层b方向的梯度
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())  # 最后一层w方向的梯度
         # Note that the variable l in the loop below is used a little
         # differently to the notation in Chapter 2 of the book.  Here,
         # l = 1 means the last layer of neurons, l = 2 is the
@@ -119,11 +120,11 @@ class Network(object):
         # that Python can use negative indices in lists.
         for l in range(2, self.num_layers):
             z = zs[-l]
-            sp = sigmoid_prime(z)
-            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
-            nabla_b[-l] = delta
-            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
-        return (nabla_b, nabla_w)
+            sp = sigmoid_prime(z)  # σ'(z)
+            delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp  # 方程[SP2]
+            nabla_b[-l] = delta  # 更新b方向的梯度 δ
+            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())  # 更新w方向的梯度 δaT
+        return nabla_b, nabla_w  # 返回代价函数关于b和w的偏导
 
     def evaluate(self, test_data):
         """Return the number of test inputs for which the neural
@@ -137,13 +138,15 @@ class Network(object):
     def cost_derivative(self, output_activations, y):
         """Return the vector of partial derivatives \partial C_x /
         \partial a for the output activations."""
-        return (output_activations-y)
+        return output_activations - y
+
 
 #### Miscellaneous functions
 def sigmoid(z):
     """The sigmoid function."""
-    return 1.0/(1.0+np.exp(-z))
+    return 1.0 / (1.0 + np.exp(-z))
+
 
 def sigmoid_prime(z):
     """Derivative of the sigmoid function."""
-    return sigmoid(z)*(1-sigmoid(z))
+    return sigmoid(z) * (1 - sigmoid(z))
